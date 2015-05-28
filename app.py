343fb4b433ea38542ffbc55ebab9eb38c52c3369
System Administrator <brian.kras@gmail.com>
2015-05-27 22:22:44 -0500

    Send card data via websocket

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from gevent import monkey
-monkey.patch_all()
+#from gevent import monkey
+#monkey.patch_all()
 
 import random
 import time
@@ -139,13 +139,91 @@ def createdeck():
 
 
 
-@socketio.on('connect', namespace='/test')
-def test_connect():
-    emit('my response', {'data': cardtest.CardName, 'count': 0})
+
 
 @socketio.on('my event', namespace='/test')
-def connect(message):
-	emit('my response', {'data': message['data']})
+def test_message(randCard):
+    cardID = randCard['randCard']
+    print cardID
+    card = (SWD.query.filter_by(id=cardID).first())
+    emit('my response', {'abbreviation': card.Abbreviation,
+ 'ability': card.Ability,
+ 'armor': card.Armor,
+ 'astromech': card.Astromech,
+ 'cancels': card.Cancels,
+ 'cardname': card.CardName,
+ 'cardnamev': card.CardNameV,
+ 'cardtype': card.CardType,
+ 'characteristics': card.Characteristics,
+ 'combo': card.Combo,
+ 'counterpart': card.Counterpart,
+ 'creature': card.Creature,
+ 'creaturedefensevalue': card.CreatureDefenseValue,
+ 'creaturedefensevaluename': card.CreatureDefenseValueName,
+ 'darksideicons': card.DarkSideIcons,
+ 'darksidetext': card.DarkSideText,
+ 'deploy': card.Deploy,
+ 'destiny': card.Destiny,
+ 'droid': card.Droid,
+ 'episode1': card.Episode1,
+ 'errata': card.Errata,
+ 'expansion': card.Expansion,
+ 'expansionv': card.ExpansionV,
+ 'exterior': card.Exterior,
+ 'ferocity': card.Ferocity,
+ 'forceaptitude': card.ForceAptitude,
+ 'forfeit': card.Forfeit,
+ 'gametext': card.Gametext,
+ 'grabber': card.Grabber,
+ 'grouping': card.Grouping,
+ 'hyperspeed': card.Hyperspeed,
+ 'icons': card.Icons,
+ 'independent': card.Independent,
+ 'influence': card.Influence,
+ 'information': card.Information,
+ 'interior': card.Interior,
+ 'inventory': card.Inventory,
+ 'iscanceledby': card.IsCanceledBy,
+ 'ispulled': card.IsPulled,
+ 'jeditestnumber': card.JediTestNumber,
+ 'landspeed': card.Landspeed,
+ 'lightsideicons': card.LightSideIcons,
+ 'lightsidetext': card.LightSideText,
+ 'lore': card.Lore,
+ 'maneuver': card.Maneuver,
+ 'matching': card.Matching,
+ 'matchingweapon': card.MatchingWeapon,
+ 'mobile': card.Mobile,
+ 'modeltype': card.ModelType,
+ 'needs': card.Needs,
+ 'objectiveback': card.ObjectiveBack,
+ 'objectivebackname': card.ObjectiveBackName,
+ 'objectivefront': card.ObjectiveFront,
+ 'objectivefrontname': card.ObjectiveFrontName,
+ 'parsec': card.Parsec,
+ 'permanentweapon': card.PermanentWeapon,
+ 'pilot': card.Pilot,
+ 'planet': card.Planet,
+ 'politics': card.Politics,
+ 'power': card.Power,
+ 'pulls': card.Pulls,
+ 'rarity': card.Rarity,
+ 'republic': card.Republic,
+ 'rules': card.Rules,
+ 'scomplink': card.ScompLink,
+ 'selectivecreature': card.SelectiveCreature,
+ 'space': card.Space,
+ 'starship': card.Starship,
+ 'subtype': card.Subtype,
+ 'tradefederation': card.TradeFederation,
+ 'underground': card.Underground,
+ 'underwater': card.Underwater,
+ 'uniqueness': card.Uniqueness,
+ 'uniquenessv': card.UniquenessV,
+ 'vehicle': card.Vehicle,
+ 'warrior': card.Warrior,
+ 'id': card.id})
+    
 
 
 
